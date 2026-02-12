80f74399210d8af4175e649474b5ec5bb692ab97
Tarren <me@tarren.moe>
2026-02-12 19:47:12 +0100

    Parse brightness properly

--- scripts/set_brightness.sh
+++ scripts/set_brightness.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 brightness=$(brightnessctl -s set $1 | grep Current | sed 's/.*(\(.*\))/\1/')
-progress=$(echo $brightness | sed 's/\(1\)\?\(..*\)%/\1.\2/')
+progress=$(echo $brightness | sed 's/100%/1/' | sed 's/\(..\)%/0.\1/' | sed 's/\(.\)%/0.0\1/')
 qs ipc call notifications specialNotification "{\"appName\":\"Brightness $brightness\",\"progress\":$progress}"
